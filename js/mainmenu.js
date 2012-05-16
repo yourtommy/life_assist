6a54df1648aa025ade4d59446b7e7a2215ef3219
Tommy Zheng <tommy_zalent@hotmail.com>
2012-05-16 18:36:44 +0800

    Use jQuery to simplify the ajax code.

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -1,20 +1,6 @@
-function openToMainContent(src)
-{
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function()
-  {
-    //if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-		$("#main_content").get(0).innerHTML=xmlhttp.responseText;
-    }
-  }
-  xmlhttp.open("GET", src, false);
-  xmlhttp.send();
-}
-
 function selectMainMenuItem(item)
 {
-	openToMainContent(item.children("a").attr("href"));
+	$("#main_content").load(item.children("a").attr("href"));
 	$("li.main_menu_current_item").attr("className", "main_menu_item");
 	item.attr("className", "main_menu_current_item");
 }
