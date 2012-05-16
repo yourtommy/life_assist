1c04eab5fa305bb9b388db61bc92db06b9c6e82e
Tommy Zheng <tommy_zalent@hotmail.com>
2012-05-17 00:56:58 +0800

    Improve the layout.
    I'm not sure whether the side-menu is good looking...

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -1,16 +1,37 @@
 function selectMainMenuItem(item)
 {
-	$("#main_content").load(item.children("a").attr("href"));
+	if (item.attr("className") != "main_menu_item")
+		return;
 	$("li.main_menu_current_item").attr("className", "main_menu_item");
 	item.attr("className", "main_menu_current_item");
+
+	$("#side_menu").load(item.attr("side-menu"), function() {
+			$("#side_menu a").mousedown(function() {
+				$(this).css({"left":"0", "opacity":"0.7"});
+			});		
+			$("#side_menu a").mouseup(function() {
+				$(this).css({"left":"-10px", "opacity":"1"});
+			});	
+			$("#side_menu a").mouseover(function() {
+				$(this).css({"left":"-10px", "opacity":"1"});
+			});	
+			$("#side_menu a").mouseleave(function() {
+				$(this).css({"left":"0", "opacity":"0.7"});
+			});
+		
+			$("#side_menu a").click(function() {
+				$("#main_content").load($(this).attr("href"));
+				return false; // disable link
+			});
+			
+			$("#desktop").slideDown("slow");
+	});
+
+	$("#main_content").load(item.children("a").attr("href"));
 }
 
 $(document).ready(function(){
-	$("li.main_menu_item").click(function() {
-		selectMainMenuItem($(this));
-	});
-	
-	$("li.main_menu_item a").click(function() {
+	$(".main_menu a").click(function() {
 		selectMainMenuItem($(this).parent("li"));
 		return false; // disable link
 	});
