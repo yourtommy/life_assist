9b019a8a0e5747dc1de22ccb6f033dbe40ba9be0
Tommy Zheng <tommy_zalent@hotmail.com>
2012-05-17 13:31:44 +0800

    Changed the side menu because I really don't like the old one.
    Although the previous side menu took me more than 5 hours...

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -6,25 +6,12 @@ function selectMainMenuItem(item)
 	item.attr("className", "main_menu_current_item");
 
 	$("#side_menu").load(item.attr("side-menu"), function() {
-			$("#side_menu a").mousedown(function() {
-				$(this).css({"left":"0", "opacity":"0.7"});
-			});		
-			$("#side_menu a").mouseup(function() {
-				$(this).css({"left":"-10px", "opacity":"1"});
-			});	
-			$("#side_menu a").mouseover(function() {
-				$(this).css({"left":"-10px", "opacity":"1"});
-			});	
-			$("#side_menu a").mouseleave(function() {
-				$(this).css({"left":"0", "opacity":"0.7"});
-			});
-		
 			$("#side_menu a").click(function() {
 				$("#main_content").load($(this).attr("href"));
 				return false; // disable link
 			});
 			
-			$("#desktop").slideDown("slow");
+			// $(".desktop").slideDown("slow");
 	});
 
 	$("#main_content").load(item.children("a").attr("href"));
